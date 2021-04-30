d0fb3392de594a26a055f82457e3466f32b54b8e
Flora2020 <rossignols1989@gmail.com>
2021-04-30 20:40:31 +0800

    Initialize vue-socket.io

--- src/components/publicChatRoom.vue
+++ src/components/publicChatRoom.vue
@@ -1,11 +1,44 @@
+<template>
+  <div id="app55">
+    <div id="nav55">
+      <button @click="connect">建立连接</button>
+      <button @click="sendMessage">发送数据</button>
+    </div>
+  </div>
+</template>
+
 <script>
-import { io } from "socket.io-client"
-const option = {
-  withCredentials: true,
-  extraHeaders: {
-    'my-custom-header': 'abcd'
+export default {
+  data () {
+    return {};
+  },
+  methods: {
+    // 连接socket
+    connect () {
+      this.$socket.open() // 开始连接socket
+      // 订阅事件
+      this.sockets.subscribe("welcome", data => {
+        console.log("welcome data==> ", data)
+      })
+    },
+
+    // 发送消息
+    sendMessage () {
+      this.$socket.emit("hello", "这里是客户端");
+    }
+  },
+
+  sockets: {
+    connect (data) {
+      console.log(data);
+    },
+    //
+    welcome: data => {
+      console.log("welcome data数据返回= >", data);
+    }
+  },
+  beforeDestroy () {
+    this.sockets.unsubscribe('welcome')
   }
-}
-const socket = io('http://localhost:3000')
-socket.emit("hello", "world")
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,33 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import AxiosPlugin from 'vue-axios-cors'
+import VueSocketIO from "vue-socket.io"
 
 Vue.config.productionTip = false
 
 Vue.use(AxiosPlugin)
+Vue.use(
+  new VueSocketIO({
+    debug: true, // debug调试，生产建议关闭
+    connection: 'http://localhost:3000'
+  })
+)
 
 new Vue({
+  sockets: {
+    connecting () {
+      console.log('正在连接');
+    },
+    disconnect () {
+      console.log('Socket 断开');
+    },
+    connect_failed () {
+      console.log('连接失败');
+    },
+    connect () {
+      console.log('socket connected');
+    }
+  },
   router,
   store,
   render: function (h) { return h(App) }
